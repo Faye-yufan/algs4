384d3bf9171dcbfc8b3c4f29b9cef57b1cf2dc2c
Faye-yufan <yufanfei@usc.edu>
2023-01-26 22:23:09 -0800

    percolation 1/26/23 submission

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,30 +4,94 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private static final int TOP = 0;
+    private final boolean[][] opened;
+    private final int size;
+    // TODO: change bottom to a list to avoid 'backwash'
+    private final int bottom;
+    private int openSites;
+    private final WeightedQuickUnionUF uf;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n)
+    public Percolation(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        size = n;
+        bottom = size * size + 1;
+        uf = new WeightedQuickUnionUF(size * size + 2);
+        opened = new boolean[size][size];
+        openSites = 0;
+    }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col)
+    public void open(int row, int col) {
+        checkException(row, col);
+        opened[row - 1][col - 1] = true;
+        openSites++;
+
+        int indexNum = getQuickFindIndex(row, col);
+        // if top row opened, union(cell, top)
+        if (row == 1) {
+            uf.union(indexNum, TOP);
+        }
+
+        if (row == size) {
+            uf.union(indexNum, bottom);
+        }
+
+        // if any of cells in the middle rows are opened then check for neighbouring unions
+        if (row > 1 && isOpen(row - 1, col)) {
+            uf.union(indexNum, getQuickFindIndex(row - 1, col));
+        }
+
+        if (row < size && isOpen(row + 1, col)) {
+            uf.union(indexNum, getQuickFindIndex(row + 1, col));
+        }
+
+        if (col > 1 && isOpen(row, col - 1)) {
+            uf.union(indexNum, getQuickFindIndex(row, col - 1));
+        }
+
+        if (col < size && isOpen(row, col + 1)) {
+            uf.union(indexNum, getQuickFindIndex(row, col + 1));
+        }
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col)
+    public boolean isOpen(int row, int col) {
+        return opened[row - 1][col - 1];
+    }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col) {
+        checkException(row, col);
+        return uf.find(TOP) == uf.find(getQuickFindIndex(row, col));
+
+    }
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites() {
+        return openSites;
+    }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates() {
+        return uf.find(TOP) == uf.find(bottom);
+    }
 
+    private void checkException(int row, int col) {
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int getQuickFindIndex(int row, int col) {
+        return size * (row - 1) + col;
+    }
     // test client (optional)
-    public static void main(String[] args)
+    // public static void main(String[] args)
 }
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,71 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private static final double CONFIDENCE_95 = 1.96;
+    private final int experimentCount;
+    private final double[] fractions;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        experimentCount = trials;
+        fractions = new double[experimentCount];
+        for (int expNum = 0; expNum < experimentCount; expNum++) {
+            Percolation pr = new Percolation(n);
+            int openedSites = 0;
+            while (!pr.percolates()) {
+                int i = StdRandom.uniformInt(1, n + 1), j = StdRandom.uniformInt(1, n + 1);
+                if (!pr.isOpen(i, j)) {
+                    pr.open(i, j);
+                    openedSites++;
+                }
+            }
+            double fraction = openedSites / (n * n);
+            fractions[expNum] = fraction;
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - CONFIDENCE_95 * stddev();
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + CONFIDENCE_95 * stddev();
+    }
+
+    public static void main(String[] args) {
+        int[] input = StdIn.readAllInts();
+        int n = input[0], t = input[1];
+
+        PercolationStats ps = new PercolationStats(n, t);
+
+        String confidence = "[" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]";
+        StdOut.println("mean                    = " + ps.mean());
+        StdOut.println("stddev                  = " + ps.stddev());
+        StdOut.println("95% confidence interval = " + confidence);
+    }
+}
